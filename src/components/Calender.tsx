ce52b2630068c9c6c8e31d9830ab7d7ebaa517cc
Henriette <henelt@outlook.com>
2024-12-09 00:13:06 +0100

    style: remove transition

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,23 +1,15 @@
 import { Typography, Paper, Stack, Button, Box, createTheme, ThemeProvider } from "@mui/material";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import christmas from '../assets/image.png'
 
 export function Calender(){
     const navigate = useNavigate();
     const buttons = Array.from({ length: 24 }, (_, index) => index + 1);
-    const [calendarZoomed, setCalendarZoomed] = useState(false);
-    const [clickedNumber, setClickedNumber] = useState<number>(0);
 
     const date = new Date();
 
     function handleClick (doorId: number){
-      setCalendarZoomed(true);
-
-      setTimeout(() => {
-        navigate('/door/' + doorId);
-      }, 500); 
-      setClickedNumber(doorId);
+      navigate('/door/' + doorId);
     }
 
     const theme = createTheme({
@@ -47,8 +39,7 @@ export function Calender(){
                   justifyContent='center' 
                   alignItems='center' 
                   flexGrow={1}>
-                <Paper sx={{backgroundImage: `url(${christmas})`,backgroundPosition: 'center', padding: 2,  backgroundRepeat: 'no-repeat', backgroundSize: 'cover', transition: 'transform 0.5s',
-                            transform: calendarZoomed ? 'scale(3)' : 'scale(1)', 
+                <Paper sx={{backgroundImage: `url(${christmas})`,backgroundPosition: 'center', padding: 2,  backgroundRepeat: 'no-repeat', backgroundSize: 'cover',
                             position: 'relative', margin: 'auto', }}>
                 {Array.from({ length: 6 }, (_, rowIndex) => (
                         <Stack direction="row" spacing={30} mb={2} justifyContent="center" key={rowIndex}>
@@ -56,7 +47,7 @@ export function Calender(){
                           <ThemeProvider theme={theme}>
                                 <Button disabled={date.getDate() < rowIndex * 4 + colIndex+1} key={colIndex} onClick={() => handleClick(rowIndex * 4 + colIndex+1)}
                                   sx={{fontSize: 40,
-                                    opacity: calendarZoomed && clickedNumber !== buttonNumber ? 0 : 1, border: 3, margin: '0',
+                                    border: 3, margin: '0',
                                     textAlign: 'center', 
                                     color: 'white', fontWeight: 'bold', 
                                     backgroundColor: 'rgba(255, 255, 255, 0.2)', 
